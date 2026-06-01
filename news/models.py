90677952cc94d55978011df305e7f74f8844107b
jyzoo <jyzoo@mail.ru>
2026-06-01 04:43:15 +0300

    lab13

--- news/models.py
+++ news/models.py
@@ -24,7 +24,8 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, verbose_name='Категория')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, verbose_name='Категория', related_name='get_news')
+    views = models.IntegerField(default=0)
 
     def get_absolute_url(self):
         return reverse('view_news',
--- news/templatetags/news_tags.py
+++ news/templatetags/news_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from ..models import Category
+from django.db.models import Count
 
 register = template.Library()
 @register.simple_tag(name='get_list_categories')
@@ -9,5 +10,5 @@ def get_categories():
 @register.inclusion_tag('news/list_categories.html')
 
 def show_categories(arg1='Последние', arg2='новости'):
-    categories = Category.objects.all()
+    categories = Category.objects.annotate(cnt=Count('get_news')).filter(cnt__gt=0)
     return {'categories': categories, 'arg1': arg1, 'arg2': arg2}
